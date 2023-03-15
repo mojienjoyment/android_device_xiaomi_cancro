18d29aa619762abe2c64af00079ae34ec688a379
Moji <68327458+mojienjoyment@users.noreply.github.com>
2023-03-16 01:25:12 +0330

    for havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cancro.mk
+    $(LOCAL_DIR)/havoc_cancro.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cancro-user \
-    lineage_cancro-userdebug \
-    lineage_cancro-eng
+    havoc_cancro-user \
+    havoc_cancro-userdebug \
+    havoc_cancro-eng
